56e90209ab219683f2692ec285303dc145275023
Seongju Moon <seongju.moon@linux.com>
2022-11-16 08:36:56 +0900

    enabled grafana option.

---
+++ ch1/1.4/1.prometheus-stack-installer-15s.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+helm install prometheus-stack edu/kube-prometheus-stack  \
+--set defaultRules.create="false" \
+--set alertmanager.enabled="false" \
+--set prometheus.service.type="LoadBalancer" \
+--set prometheus.service.port="80" \
+--set prometheus.prometheusSpec.scrapeInterval="15s" \
+--set prometheus.prometheusSpec.evaluationInterval="15s" \
+--set grafana.adminPassword="admin" \
+--set grafana.image.grafana.tag="9.2.4" \
+--set grafana.service.type="LoadBalancer" \
+--set grafana.persistence.enabled="true" \
+--namespace=monitoring \
+--create-namespace \
+-f ~/_Lecture_graf_learning.kit/ch1/1.4/prom-operator-config/prom-operator-config-merged.yaml
