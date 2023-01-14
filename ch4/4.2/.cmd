bf8bdbd39992a29eb6a29e832cb69b97c4510707
Seongju Moon <seongju.moon@linux.com>
2023-01-15 02:16:05 +0900

    add verifiy lab

---
+++ ch4/4.2/.cmd
@@ -0,0 +1,23 @@
+# create configmap for change dashboard pannel data change
+k create configmap db-hostinfo --from-file host-info.conf
+configmap/db-hostinfo created
+
+# check configmap resource 
+
+[root@m-k8s ch4]# k get cm db-connection-info -o yaml
+apiVersion: v1
+data:
+  host-info.conf: |
+    DB_HOST=database.default.svc.cluster.local
+    DB_PORT=3306
+    DB_DATABASE_NAME=service
+kind: ConfigMap
+metadata:
+  ...
+  name: db-hostinfo
+  namespace: default
+  ...
+
+# delete configmap for change dashboard pannel data change
+k delete configmap db-hostinfo
+configmap "db-hostinfo" deleted
