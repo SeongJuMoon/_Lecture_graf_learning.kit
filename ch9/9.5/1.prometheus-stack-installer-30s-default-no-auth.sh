7bdc814e2a02318ecde2b2dc8494e447ffea1c2b
Seongju Moon <seongju.moon@linux.com>
2023-01-28 03:55:26 +0900

    rearrange

--- ch9/9.5/1.prometheus-stack-installer-30s-default-no-auth.sh
+++ ch9/9.5/1.prometheus-stack-installer-30s-default-no-auth.sh
@@ -13,8 +13,10 @@ helm install prometheus-stack edu/kube-prometheus-stack \
 --set grafana.service.type="LoadBalancer" \
 --set grafana.service.loadBalancerIP="192.168.1.12" \
 --set grafana.persistence.enabled="true" \
---set grafana."grafana\.ini".server.domain="192.168.1.12" \
+--set grafana."grafana\.ini"."auth\.anonymous".enabled="true" \
+--set grafana."grafana\.ini"."auth\.anonymous".org_name="Main Org." \
+--set grafana."grafana\.ini"."auth\.anonymous".org_role="Admin" \
 --set grafana."grafana\.ini".server.root_url="http://192.168.1.12" \
 --namespace=monitoring \
 --create-namespace \
--f ~/_Lecture_graf_learning.kit/ch9/9.2/prom-operator-config/prom-operator-config-merged.yaml
+-f ~/_Lecture_graf_learning.kit/ch9/9.5/prom-operator-config/prom-operator-config-merged.yaml
