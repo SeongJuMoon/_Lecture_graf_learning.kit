23d516998e92759a8afd7a0e753a92243e8a38ab
Hoon Jo <pagaia@hotmail.com>
2023-02-27 10:42:40 +0900

    upl stress sh

--- ch5/5.6/.cmd
+++
@@ -1,2 +0,0 @@
-# from w3-k8s 
-  stress --vm 2 --vm-bytes 512m --timeout 60s
---
+++ ch5/5.6/stress-vm-60s-on-w3-k8s.sh
@@ -0,0 +1,4 @@
+# from w3-k8s
+# auth:  yes
+# password: vagrant
+ssh root@w3-k8s "stress --vm 2 --vm-bytes 512m --timeout 60s"
