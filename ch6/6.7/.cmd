d3bf109ee2c1b1df68e802cc114633100024d294
Hoon Jo <pagaia@hotmail.com>
2023-03-05 11:15:57 +0900

    upt .cmd

--- ch6/6.7/.cmd
+++ ch6/6.7/.cmd
@@ -1,14 +1,14 @@
 0. install dependecies loki
 ./loki-installer.sh
 
-1. check loki deployment is ready 
+1.check loki deployment is ready 
 k get po -l app=loki -n monitoring
 ---
 [root@m-k8s 1.3]# k get po -l app=loki -n monitoring
 NAME           READY   STATUS    RESTARTS   AGE
 loki-stack-0   1/1     Running   0          1m
 
-2. check loki service endpoint
+2.check loki service endpoint
 k get svc -l app=loki -n monitoring
 ----
 [root@m-k8s 1.3]# k get svc -n monitoring -l app=loki
@@ -17,10 +17,11 @@ loki-stack              ClusterIP   10.104.112.228   <none>        3100/TCP   9m
 loki-stack-headless     ClusterIP   None             <none>        3100/TCP   9m46s
 loki-stack-memberlist   ClusterIP   None             <none>        7946/TCP   9m46s
 
-3. setting datasource at grafana
+3.setting datasource in grafana
 
-4. paint log pannel in dashboard.
+4.draw `log` pannel in the dashboard
+ - Data source: loki 
+ - log code: {app="nginx"} |= "error"
 
-5. uninstall loki and nginx (optional)
-k delete -f ./1.nginx
-./2.loki-uninstaller.sh
+5.uninstall loki and nginx 
+k delete -f 2.nginx-n-loki-uninstaller.sh
