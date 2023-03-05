08ef810400b5ae2353da6997daa546c451c797b5
Hoon Jo <pagaia@hotmail.com>
2023-03-05 10:42:02 +0900

    upt .cmd

--- ch6/6.6/.cmd
+++ ch6/6.6/.cmd
@@ -1,5 +1,11 @@
 apiserver_request_duration_seconds_bucket{verb="GET"}
 #Grafana의 suggestion을 누르고 rate값을 1m으로 수정 
 
-histogram_quantile(0.01, sum(rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m])) by (le))
-histogram_quantile(0.95, sum(rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m])) by (le))
+#histogram_quantile(0.01, sum(rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m])) by (le))
+#histogram_quantile(0.95, sum(rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m])) by (le))
+
+histogram_quantile(0.01, rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m]))
+histogram_quantile(0.20, rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m]))
+histogram_quantile(0.95, rate(apiserver_request_duration_seconds_bucket{verb="GET"}[1m]))
+
+# Change Thresholds according to ms value(current 0.05s = 50.000ms)
