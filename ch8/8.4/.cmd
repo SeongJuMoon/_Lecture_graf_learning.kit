6db0d2b8478fb1a8eae45c3747ce81ed6195aaa2
Hoon Jo <pagaia@hotmail.com>
2023-03-13 16:16:10 +0900

    upt .cmd

--- ch8/8.4/.cmd
+++ ch8/8.4/.cmd
@@ -1,13 +1,12 @@
 # pre-requirement 
+
 ## deploy jaeger 
 k apply -f 0.jaeger/manifests/
+## check 
+k get po,svc -n monitoring
+
 ## deploy hotrod application 
 k apply -f 0.jaeger/example/hotrod-🚗 
-
-# after drawing Canvas panel 
-k scale -n ingress-nginx deployment ingress-nginx-controller --replicas=0
-k scale -n ingress-nginx deployment ingress-nginx-controller --replicas=1
-
-# delete deployed app
-k delete -f 0.ingress_ctrl_loadbalancer.yaml
+## check 
+k get po,svc 
 
