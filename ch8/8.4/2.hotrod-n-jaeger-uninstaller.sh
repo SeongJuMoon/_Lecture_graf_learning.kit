1eee85ae3d3dd25ccd3611917cafb8cd22395711
Hoon Jo <pagaia@hotmail.com>
2023-03-15 10:14:28 +0900

    rearrange jaeger app

---
+++ ch8/8.4/2.hotrod-n-jaeger-uninstaller.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/0.jaeger/example/hotrod-🚗/
+kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/0.jaeger/manifests/
+
