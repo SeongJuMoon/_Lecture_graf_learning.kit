a4c26d69c2583dabdd31ff529e9021558eaeb715
Hoon Jo <pagaia@hotmail.com>
2023-03-15 14:30:16 +0900

    chg name and misc

--- A/A.001/prometheus+grafana-force-installer.sh
+++ A/A.001/prometheus+grafana-force-installer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 helm uninstall -n monitoring prometheus > /dev/null 2>&1
+helm uninstall -n monitoring grafana > /dev/null 2>&1
 helm uninstall -n monitoring prometheus-stack > /dev/null 2>&1
 kubectl -n monitoring delete pvc prometheus-prometheus-stack-kube-prom-prometheus-db-prometheus-prometheus-stack-kube-prom-prometheus-0 > /dev/null 2>&1
 echo "waiting for deleting prometheus componentes in 5seconds"; sleep 5
