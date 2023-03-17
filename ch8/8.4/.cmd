4f42ef0a97b352bd32b7f7823fd8d39fcce2a2ec
Hoon Jo <pagaia@hotmail.com>
2023-03-17 10:15:09 +0900

    rearrange all

--- ch8/8.4/.cmd
+++ ch8/8.4/.cmd
@@ -1,12 +1,14 @@
 # pre-requirement 
 
 ## deploy jaeger 
-k apply -f 0.jaeger/manifests/
+k apply -f 0.jaeger
 ## check 
 k get po,svc -n monitoring
 
 ## deploy hotrod application 
-k apply -f 0.jaeger/example/hotrod-🚗 
+k apply -f 1.example-hotrod-🚗
 ## check 
 k get po,svc 
 
+## uninstall all of applications 
+./2.hotrod-n-jaeger-uninstaller.sh
--- ch8/8.4/2.hotrod-n-jaeger-uninstaller.sh
+++ ch8/8.4/2.hotrod-n-jaeger-uninstaller.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/0.jaeger/example/hotrod-🚗/
-kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/0.jaeger/manifests/
+kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/1.example-hotrod-🚗/
+kubectl delete -f ~/_Lecture_graf_learning.kit/ch8/8.4/0.jaeger
 
