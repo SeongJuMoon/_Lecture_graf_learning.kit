03f456d72c6fa2f6aa1dd92f5adab6b49df94744
Hoon Jo <pagaia@hotmail.com>
2023-03-17 14:54:56 +0900

    rename

--- ch8/8.3/.cmd
+++ ch8/8.3/.cmd
@@ -1,10 +1,10 @@
-# pre-requirement 
-k apply -f 0.ingress_ctrl_loadbalancer.yaml
+# deploy ingress_ctrl for testing purpose
+k apply -f ingress_ctrl_loadbalancer.yaml
 
 # after drawing Canvas panel 
 k scale -n ingress-nginx deployment ingress-nginx-controller --replicas=0
 k scale -n ingress-nginx deployment ingress-nginx-controller --replicas=1
 
 # delete deployed app
-k delete -f 0.ingress_ctrl_loadbalancer.yaml
+k delete -f ingress_ctrl_loadbalancer.yaml
 
