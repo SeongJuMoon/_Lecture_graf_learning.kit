c4a107aec53dbd0f56cd485caa0928c4aa7340f8
Seongju Moon <seongju.moon@linux.com>
2023-04-01 13:29:36 +0900

    add dashboard json and fix cmd description.

--- ch9/9.5/.cmd
+++ ch9/9.5/.cmd
@@ -4,7 +4,7 @@ k create deploy nginx --image=nginx:stable --replicas=3
 # 1. To simulate outage, scaled deploy nginx replicas down to 1.
 k scale deploy nginx --replicas 1
 
-# 2. Restore simulate outage, scaled deploy nginx replicas up to 5.
+# 2. Restore simulate outage, scaled deploy nginx replicas up to 3.
 k scale deploy nginx --replicas 3
 
 # 3. Delete nginx deployment.
