4a39cd6f01c11e60e32f3a5d3e6b008689e99a3b
Seongju Moon <seongju.moon@linux.com>
2023-05-01 09:05:16 +0900

    add loki and jaeger to visualize dashboard.

---
+++ ch9/9.8/1.loki-n-jaeger-installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "1. loki install via helm."
+
+helm install loki-stack edu/loki-stack \
+--set grafana.enabled=false \
+--namespace monitoring 
+
+echo "2. nginx install via kubectl."
+kubectl apply -f nginx ~/_Lecture_graf_learning.kit/ch9/9.8/nginx
+
+echo "3.jaeger and hotrod install via kubectl."
+kubectl apply -f ~/_Lecture_graf_learning.kit/ch9/9.8/jaeger
+
+echo "loki & jaeger datasource install successfully."
