3b6f5595fd8985ee872c173378066975ebc35d49
Seongju Moon <seongju.moon@linux.com>
2023-05-20 21:39:27 +0900

    update scenario

--- A/A.004/.cmd
+++ A/A.004/.cmd
@@ -4,28 +4,27 @@
 # <shoutcut> http://192.168.1.12/admin/users
 
 2-1. Press right upside New User button. to create new user.
-# Name: Alice
-# UserName: Alice
-# Password: Alice
-# Click Create User to create Alice.
+# Name: seongju
+# UserName: seongju
+# Password: seongju
+# Click Create User to create seongju.
 # Check privileges:	Viewer
 
 2-2. Press right upside New User button. to create new user.
-# Name: Bob
-# UserName: Bob
-# Password: BobBob
-# Caused by: The password required least 4 word.
-# Click Create User to Bob Alice.
+# Name: hoon
+# UserName: hoon
+# Password: hoon
+# FYI: The password required least 4 word.
+# Click Create User to seongju and hoon.
 # Check privileges:	Viewer
 
-2-3. Open browser congnito mode, Conect grafana ui.
+2-3. Open browser congnito mode, Connect grafana ui.
 # <shoutcut> http://192.168.1.12/
-# Login to Alice, check successfully logined grafana.
-# Bob to Alice, check successfully logined grafana.
+# Login to seongju, check successfully logined grafana.
+# Login to hoon, check successfully logined grafana.
 
 2-4. Check Server admin page.
 # Check Last active chanaged.
-
 
 3-1. Click Configruation/Team menu to Create Team.
 # <shoutcut> http://192.168.1.12/org/teams
@@ -33,10 +32,10 @@
 # Name: 운영관리팀
 # Click Create.
 
-3-2. Add Bob and Alice into 운영관리팀.
+3-2. Add hoon and seongju into 운영관리팀.
 # Click Add member.
-# Choose User and Alice and select member at generated ui after click save button. 
-# Choose User and Bob and select member at generated ui after click save button.
+# Choose User and seongju and select member at generated ui after click save button. 
+# Choose User and hoon and select member at generated ui after click save button.
 # Click x button to disposing generated ui.
 # Check All member added into 운영관리팀.
 
@@ -49,7 +48,7 @@
 # Click Import.
 
 4-2 Check Permission `ch5.그라파나 패널에서 제공되는 다양한 옵션들`.
-# Login to Alice. Check The dashboard is not editable.
+# Login to seongju. Check The dashboard is not editable.
 
 4-3. Grant `ch5.그라파나 패널에서 제공되는 다양한 옵션들` dashboard at 운영관리팀.
 # Choose target dashboard we recomm to `ch5.그라파나 패널에서 제공되는 다양한 옵션들`
@@ -60,11 +59,11 @@
 # Check dashboard permission into `ch5.그라파나 패널에서 제공되는 다양한 옵션들`.
 
 4-4 Check Permission `ch5.그라파나 패널에서 제공되는 다양한 옵션들`.
-# Login to Alice. Check The dashboard is editable.
+# Login to seongju. Check The dashboard is editable.
 
-4-5. remove Alice at 운영관리팀.
+4-5. remove seongju at 운영관리팀.
 # <shoutcut> http://192.168.1.12/org/teams
-# Click X Button, Row at Alice
+# Click X Button, Row at seongju, to remove team.
 
 4-6 Check Permission `ch5.그라파나 패널에서 제공되는 다양한 옵션들`.
-# Login to Alice. Check The dashboard is not editable.
+# Login to seongju. Check The dashboard is not editable.
