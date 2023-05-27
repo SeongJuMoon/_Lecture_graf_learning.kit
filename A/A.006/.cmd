a7bfda5668dd9164e9805fca816aab0a7ffcf116
Seongju Moon <seongju.moon@linux.com>
2023-05-27 13:13:30 +0900

    reset password to sql.

--- A/A.006/.cmd
+++ A/A.006/.cmd
@@ -76,3 +76,6 @@ select * from user;
 select * from dashboard;
 select * from data_source;
 
+# 3-5 change password from 
+cat 3.admin-password-reset.sql
+update user set password = '59acf18b94d7eb0694c61e60ce44c110c7a683ac6a8f09580d626f90f4a242000746579358d77dd9e570e83fa24faa88a8a6', salt = 'F3FAxVm33R' where login = 'admin';
