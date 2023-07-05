50c3904d9a9f0e8832298b4569cf489fa5bfc5ad
Seongju Moon <seongju.moon@linux.com>
2023-07-05 21:30:15 +0900

    rearrange

--- A/A.007/.cmd
+++ A/A.007/.cmd
@@ -1,1 +1,9 @@
-(WIP)
+#1. install mysql-client for provision test dataset.
+./0.mysql-client-installer.sh
+#2. deploy mysql deployment and service
+kubectl apply -f ./mysql.yaml
+#3. connect to mysql instance to test connection is reachable.
+mysql -h 192.168.1.97 -u root -p
+# enter the password: root
+#4. provision dataset from initialize script.
+1.initialize-testdata.sh
