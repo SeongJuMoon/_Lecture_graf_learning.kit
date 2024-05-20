2927d9c087ebbaacba82c9f50f1e0b3e09961c73
Hoon Jo <pagaia@hotmail.com>
2024-05-20 11:05:30 +0900

    upt k8s-v1.30

--- ch2/2.1/controlplane_node.sh
+++ ch2/2.1/controlplane_node.sh
@@ -5,7 +5,7 @@ kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
              --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.10 \
              --cri-socket=unix:///run/containerd/containerd.sock
 
-# config for master node only 
+# config for control-plane node only 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
@@ -22,23 +22,24 @@ echo 'alias k=kubectl' >> ~/.bashrc
 echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
-# git clone cicd-code
-git clone https://github.com/sungmincs/_Lecture_cicd_learning.kit
-mv /home/vagrant/_Lecture_cicd_learning.kit $HOME
-find $HOME/_Lecture_cicd_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+# git clone prom-code
+git clone https://github.com/SeongJuMoon/_Lecture_graf_learning.kit.git
+mv /home/vagrant/_Lecture_graf_learning.kit $HOME
+find $HOME/_Lecture_graf_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 
-# make rerepo-cicd-learning.kit and put permission
-cat <<EOF > /usr/local/bin/rerepo-cicd-learning.kit
+# make rerepo-graf_learning.kit and put permission
+cat <<EOF > /usr/local/bin/rerepo-graf_learning.kit
 #!/usr/bin/env bash
-rm -rf $HOME/_Lecture_cicd_learning.kit 
-git clone https://github.com/sungmincs/_Lecture_cicd_learning.kit $HOME/_Lecture_cicd_learning.kit
-find $HOME/_Lecture_cicd_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+rm -rf $HOME/_Lecture_graf_learning.kit 
+git clone https://github.com/seongjumoon/_Lecture_graf_learning.kit.git $HOME/_Lecture_graf_learning.kit
+find $HOME/_Lecture_graf_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 EOF
-chmod 700 /usr/local/bin/rerepo-cicd-learning.kit
+chmod 700 /usr/local/bin/rerepo-graf_learning.kit
 
 # extended k8s certifications all
 git clone https://github.com/yuyicai/update-kube-cert.git /tmp/update-kube-cert
 chmod 755 /tmp/update-kube-cert/update-kubeadm-cert.sh
 /tmp/update-kube-cert/update-kubeadm-cert.sh all --cri containerd
 rm -rf /tmp/update-kube-cert
+echo "Wait 10 seconds for restarting the Control-Plane Node..." ; sleep 10
 
